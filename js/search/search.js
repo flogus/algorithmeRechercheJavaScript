3910dea0fa8e3a6fb754b80eef4b7dcaa1c70e2f
flogus <fg@motionery.com>
2022-06-27 18:39:48 +0200

    fix: correct the search method

--- js/search/search.js
+++ js/search/search.js
@@ -1,5 +1,14 @@
 let results = new Array();
 function search() {
+  //Add data to results
+  recipes.forEach((element) => {
+    results.push(element.name);
+    results.push(element.description);
+    element.ingredients.forEach((ingredients) => {
+      results.push(ingredients.ingredient);
+    });
+  });
+
   const mainSearch = document.getElementById("mainsearch");
 
   mainSearch.addEventListener("keyup", doTheSearch, false);
@@ -7,16 +16,6 @@ function search() {
 
   function doTheSearch(event) {
     if (mainSearch.value.length > 2) {
-      console.log("value", mainSearch.value);
-
-      //Add data to results
-      recipes.forEach((element) => {
-        results.push(element.name);
-        results.push(element.description);
-        element.ingredients.forEach((ingredients) => {
-          results.push(ingredients.ingredient);
-        });
-      });
       const isFound = results.filter((element) => {
         if (element.indexOf(mainSearch.value) !== -1) {
           return true;
