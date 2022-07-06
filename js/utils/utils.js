29cbdee6705828c051983f9071a1947175a40169
flogus <fg@motionery.com>
2022-07-06 16:47:17 +0200

    feat: add a new function searchableWords

--- js/utils/utils.js
+++ js/utils/utils.js
@@ -1,3 +1,12 @@
 function capitalizeFirstLetter(string) {
   return string[0].toUpperCase() + string.slice(1);
 }
+function searchableWords(string) {
+  const str = string
+    .toString()
+    .trim()
+    .toLowerCase()
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "");
+  return str;
+}
