bcc0569692cdf84fc4a726ad11320647799b1eab
flogus <fg@motionery.com>
2022-07-11 15:55:30 +0200

    feat: update the search of the ingredients

--- js/search/dropdowns.js
+++ js/search/dropdowns.js
@@ -64,29 +64,99 @@ async function renderDropDowns() {
 
     let dropDown = new DropDown(element, currentList[index]);
     dropDownsContainer.innerHTML += dropDown.getDropDown();
-    dropDownSearchBox(searchableWords(element));
+    setTimeout(() => {
+      dropDownSearchBox("search-" + searchableWords(element));
+    }, 100);
   });
+  // dropDownSearchBoxes();
 }
-
 function dropDownSearchBox(type) {
-  const searchBox = document.getElementById("search" + type);
-  searchBox.addEventListener("keyup", function (e) {
-    searchList(searchBox.value, type);
+  var searchBoxId = document.getElementById(type);
+  searchBoxId.addEventListener("keyup", function (e) {
+    console.log(e.key);
+    renderSearchList(type);
   });
 }
+function dropDownSearchBoxes() {
+  // let searchId = "search-" + type;
 
-function searchList(searchBoxValue, type) {
+  document
+    .getElementById("search-ustensile")
+    .addEventListener("keyup", function (e) {
+      console.log("KeyUp", e.key);
+    });
+
+  document
+    .getElementById("search-ingredient")
+    .addEventListener("keyup", function (e) {
+      console.log("KeyUp", e.key);
+    });
+
+  document
+    .getElementById("search-appareil")
+    .addEventListener("keyup", function (e) {
+      console.log("KeyUp", e.key);
+      //searchList(searchBox.value, type);
+    });
+
+  // var ser = "search-ingredient";
+  // var searchBox2 = document.getElementById(ser);
+  // searchBox2.addEventListener("keyup", function (e) {
+  //   console.log("keyup", e.key);
+  // });
+}
+
+function getSearchList(type) {
+  var searchBoxValue = document.getElementById(type).value;
+  var typeName = type.split("-").pop();
+
   const dropContainerCollection = document.getElementById(
-    "dropDownMenu-" + type
+    "dropDownMenu-" + typeName
   ).firstElementChild.children;
 
-  eval(type + "ListLow").forEach(function (element, index) {
+  let listLow = "";
+  if (typeName == "ingredient") {
+    listLow = getFilterDropDowns()[3];
+  }
+  if (typeName == "appareil") {
+    listLow = getFilterDropDowns()[4];
+  }
+  if (typeName == "ustensile") {
+    listLow = getFilterDropDowns()[5];
+  }
+  // console.log("listLow", listLow);
+  let tempListLow = new Array();
+
+  // Loop on every elemts of the current list (ingredient, appareil or ustensile)
+  eval(listLow).forEach(function (element, index) {
     //Not found then hide
     if (searchBoxValue.length > 2) {
-      if (!element.includes(searchBoxValue)) {
-        if (dropContainerCollection[index] != null) {
-          dropContainerCollection[index].classList.add("d-none");
-        }
+      // Is the searchBoxValue is in the list
+      if (element.includes(searchBoxValue)) {
+        console.info("FOUND", index, element, searchBoxValue);
+
+        // Search in recipes the ingredient of the following element
+        recipes.forEach(function (recipe) {
+          Object.entries(recipe).forEach((entry) => {
+            const [key, value] = entry;
+            if (typeName == "ingredient") {
+              if (key == "ingredients") {
+                value.forEach(function (val) {
+                  // if found once add all ingredients
+                  if (searchableWords(val.ingredient).includes(element)) {
+                    value.forEach(function (val) {
+                      if (
+                        !tempListLow.includes(searchableWords(val.ingredient))
+                      ) {
+                        tempListLow.push(searchableWords(val.ingredient));
+                      }
+                    });
+                  }
+                });
+              }
+            }
+          });
+        });
       }
     } else {
       if (dropContainerCollection[index] != null) {
@@ -94,6 +164,31 @@ function searchList(searchBoxValue, type) {
       }
     }
   });
+  console.info("tempListLow", tempListLow);
+  return tempListLow;
+}
+
+function renderSearchList(type) {
+  var searchBoxValue = document.getElementById(type).value;
+
+  console.log("type", type);
+  var typeName = type.split("-").pop();
+  const currentList = getSearchList(type);
+
+  const dropContainerCollection = document.getElementById(
+    "dropDownMenu-" + typeName
+  ).firstElementChild.children;
+
+  if (searchBoxValue.length > 2) {
+    for (let item of dropContainerCollection) {
+      const currentTextLink = searchableWords(item.children[0].innerText);
+      if (!currentList.includes(currentTextLink)) {
+        item.classList.add("d-none");
+      } else {
+        item.classList.remove("d-none");
+      }
+    }
+  }
 }
 
 function filterAndRenderDropdowns() {
