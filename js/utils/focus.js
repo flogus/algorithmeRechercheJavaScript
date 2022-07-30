9ebc3f3006146437dfcc345eabd8216bc5db81e5
flogus <fg@motionery.com>
2022-07-30 15:40:18 +0200

    fix: removed old commented code

--- js/utils/focus.js
+++ js/utils/focus.js
@@ -2,21 +2,6 @@
  * Focus on the searchboxes
  */
 function doFocus() {
-  // $("#ingredient-container").on("show.bs.dropdown", function () {
-  //   setTimeout(() => {
-  //     $("#search-ingredient").focus();
-  //   }, 0);
-  // });
-  // $("#appareil-container").on("show.bs.dropdown", function () {
-  //   setTimeout(() => {
-  //     $("#search-appareil").focus();
-  //   }, 0);
-  // });
-  // $("#ustensile-container").on("show.bs.dropdown", function () {
-  //   setTimeout(() => {
-  //     $("#search-ustensile").focus();
-  //   }, 0);
-  // });
   setTimeout(() => {
     $("#mainsearch").focus();
   }, 50);
